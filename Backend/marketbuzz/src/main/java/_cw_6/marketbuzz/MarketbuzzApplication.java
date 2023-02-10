ac76dd3f93a4f9ede67638e6384da3451b5b5d7e
bpodjenski <benpo@iastate.edu>
2023-02-10 15:48:12 -0600

    initial project setup for springboot backend

---
+++ Backend/marketbuzz/src/main/java/_cw_6/marketbuzz/MarketbuzzApplication.java
@@ -0,0 +1,13 @@
+package _cw_6.marketbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketbuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketbuzzApplication.class, args);
+	}
+
+}
---
+++ Backend/marketbuzz/src/test/java/_cw_6/marketbuzz/MarketbuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package _cw_6.marketbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketbuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
