6a0507947233cc93a2b8dd14a082bbbde0fe1804
josephTJennings <josephjenningsct@gmail.com>
2023-03-01 15:52:35 -0600

    Fixed issue with mrge imports.

--- Frontend/app/src/main/java/com/example/stockproject/Activities/MainActivity.java
+++ Frontend/app/src/main/java/com/example/stockproject/Activities/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.stockproject.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.example.stockproject.R;
 
 public class MainActivity extends AppCompatActivity {
