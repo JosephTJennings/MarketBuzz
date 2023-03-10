5e8e9792f8f7466bac5ebd776264cd4ee71015bb
samhead2001 <srhusted@iastate.edu>
2023-03-10 01:49:59 -0600

    Edited activity_main to have all buttons we plan to implement into the project.

--- Frontend/app/src/main/java/com/example/stockproject/Activities/MainActivity.java
+++ Frontend/app/src/main/java/com/example/stockproject/Activities/MainActivity.java
@@ -10,20 +10,74 @@ import com.example.stockproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button leaderboard;
-    private String currentUser;
+    private Button leaderboard, logout, stocks, profile, followers, options;
+    private TextView user, money;
+    private String currentUser, currentMoney;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         currentUser = getIntent().getStringExtra("username");
-        System.out.println(currentUser);
+//        System.out.println(currentUser);
+        leaderboard = (Button) findViewById(R.id.leaderboard_page);
+        followers = (Button) findViewById(R.id.followers_page);
+        logout = (Button) findViewById(R.id.logout_page);
+        stocks = (Button) findViewById(R.id.stocks_page);
+        profile = (Button) findViewById(R.id.profile_page);
+        options = (Button) findViewById(R.id.options_page);
+        user = (TextView) findViewById(R.id.currentUser);
+        //Append currentUser to user
+        user.setText(currentUser);
+        //TODO: Add a get request for the current funds the user has
+        money = (TextView) findViewById(R.id.currentMoney);
 
-        Button followersButton = (Button) findViewById(R.id.followers_page);
-        followersButton.setOnClickListener(new View.OnClickListener(){
+
+        followers.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), FollowersActivity.class);
+                intent.putExtra("username", currentUser);
+                //System.out.println("received and passing back: " + currentUser);
+                startActivity(intent);
+            }
+        });
+        leaderboard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LeaderboardActivity.class);
+                intent.putExtra("username", currentUser);
+                startActivity(intent);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        options.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), OptionsActivity.class);
+                intent.putExtra("username", currentUser);
+                //System.out.println("received and passing back: " + currentUser);
+                startActivity(intent);
+            }
+        });
+        profile.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                intent.putExtra("username", currentUser);
+                //System.out.println("received and passing back: " + currentUser);
+                startActivity(intent);
+            }
+        });
+        stocks.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StocksActivity.class);
                 intent.putExtra("username", currentUser);
                 //System.out.println("received and passing back: " + currentUser);
                 startActivity(intent);
---
+++ Frontend/app/src/main/java/com/example/stockproject/Activities/StocksActivity.java
@@ -0,0 +1,4 @@
+package com.example.stockproject.Activities;
+
+public class StocksActivity {
+}
