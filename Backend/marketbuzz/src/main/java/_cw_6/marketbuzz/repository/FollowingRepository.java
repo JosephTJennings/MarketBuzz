22b620d4b73e57a0d5e8e838fb885fece3c2d4c9
josephTJennings <josephjenningsct@gmail.com>
2023-03-12 18:21:55 -0500

    Possible fix for backend Following. Added a query

--- Backend/marketbuzz/src/main/java/_cw_6/marketbuzz/repository/FollowingRepository.java
+++ Backend/marketbuzz/src/main/java/_cw_6/marketbuzz/repository/FollowingRepository.java
@@ -2,7 +2,11 @@ package _cw_6.marketbuzz.repository;
 
 import _cw_6.marketbuzz.model.Following;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import _cw_6.marketbuzz.model.Person;
+import java.util.List;
 
 public interface FollowingRepository extends JpaRepository<Following, Long> {
-
+    @Query("SELECT username FROM Following WHERE username.pid = :#{user.username}") public List<Person> getUsersFollowings(Person user);
+    //NO IDEA IF THIS WORKS
 }
