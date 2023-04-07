bc704a239a2d9984c611c977548dddfb97752fe9
Joseph Jennings <jtj9@iastate.edu>
2023-04-07 14:49:48 +0000

    Update MarketbuzzApplicationTests.java

--- Backend/marketbuzz/src/test/java/_cw_6/marketbuzz/MarketbuzzApplicationTests.java
+++ Backend/marketbuzz/src/test/java/_cw_6/marketbuzz/MarketbuzzApplicationTests.java
@@ -2,7 +2,7 @@ package _cw_6.marketbuzz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//restarting runners
 @SpringBootTest
 class MarketbuzzApplicationTests {
 
