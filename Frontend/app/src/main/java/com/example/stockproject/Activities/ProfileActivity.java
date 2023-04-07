79743220a44879484b63b90e0e66a006b5e859ab
josephTJennings <josephjenningsct@gmail.com>
2023-04-06 20:19:10 -0500

    Added requests for data and implemented adapter and recycler view

--- Frontend/app/src/main/java/com/example/stockproject/Activities/ProfileActivity.java
+++ Frontend/app/src/main/java/com/example/stockproject/Activities/ProfileActivity.java
@@ -7,15 +7,24 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.RequestQueue;
 import com.example.stockproject.R;
+
+import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
     private Button HomeButton;
+    private RequestQueue volleyQueue;
+    private RecyclerView recyclerView;
+    private ArrayList<HoldingsModel> currentHoldings = new ArrayList<>();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         TextView userName = findViewById(R.id.text_username);
+        recyclerView = findViewById(R.id.holdingRecycler);
         //userName.setText(getIntent().getStringExtra("username"));
         userName.setText("TEMP");
 
@@ -47,6 +56,59 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        setCurrentHoldings();
+        //refreshRecyclerView();
+        System.out.println("fml");
+    }
 
+    public void setCurrentHoldings() {
+        for(int i = 0; i < 10; i++) {
+            int rank = 1;
+            String ticker = "MSFT";
+            int price = 300;
+            int quantity = 15;
+            int total = quantity * price;
+
+            HoldingsModel newHolding = new HoldingsModel(rank, ticker, price, quantity, total);
+            currentHoldings.add(newHolding);
+        }
+        /**
+        String url = "http://coms-309-019.class.las.iastate.edu:8080/people/stocks";
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for(int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject holding = response.getJSONObject(i);
+                                System.out.println("JSON object received");
+
+                                int rank = 1;
+                                String ticker = holding.getString("ticker");
+                                int price = Integer.valueOf(holding.getString("price"));
+                                int quantity = Integer.valueOf(holding.getString("quantity"));
+                                int total = quantity * price;
+
+                                HoldingsModel newHolding = new HoldingsModel(ticker, price, quantity);
+                                currentHoldings.add(newHolding);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        volleyQueue.add(request);
+         **/
+    }
+
+    public void refreshRecyclerView() {
+        holdings_recyclerView_adapter adapter = new holdings_recyclerView_adapter(ProfileActivity.this, currentHoldings);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(ProfileActivity.this));
     }
 }
--- Frontend/app/src/main/java/com/example/stockproject/Activities/ViewedUserActivity.java
+++ Frontend/app/src/main/java/com/example/stockproject/Activities/ViewedUserActivity.java
@@ -7,16 +7,33 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.stockproject.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ViewedUserActivity extends AppCompatActivity {
     private Button HomeButton;
+    private RequestQueue volleyQueue;
+    private RecyclerView recyclerView;
+    private ArrayList<HoldingsModel> currentHoldings = new ArrayList<>();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vieweduser);
         TextView userName = findViewById(R.id.text_username);
-        userName.setText(getIntent().getStringExtra("targetUser"));
+        //userName.setText(getIntent().getStringExtra("username"));
+        userName.setText("TEMP");
 
         HomeButton = (Button) findViewById(R.id.home_button01);
         HomeButton.setOnClickListener(new View.OnClickListener(){
@@ -28,4 +45,38 @@ public class ViewedUserActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void setCurrentHoldings() {
+        String url = "http://coms-309-019.class.las.iastate.edu:8080/people/stocks";
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for(int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject holding = response.getJSONObject(i);
+                                System.out.println("JSON object received");
+
+                                int rank = 1;
+                                String ticker = holding.getString("ticker");
+                                int price = Integer.valueOf(holding.getString("price"));
+                                int quantity = Integer.valueOf(holding.getString("quantity"));
+                                int total = quantity * price;
+
+                                HoldingsModel newHolding = new HoldingsModel(rank, ticker, price, quantity, total);
+                                currentHoldings.add(newHolding);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        volleyQueue.add(request);
+    }
+
 }
