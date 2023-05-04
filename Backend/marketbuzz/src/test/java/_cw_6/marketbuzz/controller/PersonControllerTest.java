bb856060f297ba20464041cee0cbcc6ecaa1657b
JosephTJennings <josephjenningsct@gmail.com>
2023-05-04 00:45:45 -0500

    Added test for creating and deleting a user!

--- Backend/marketbuzz/src/test/java/_cw_6/marketbuzz/controller/PersonControllerTest.java
+++ Backend/marketbuzz/src/test/java/_cw_6/marketbuzz/controller/PersonControllerTest.java
@@ -13,6 +13,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
@@ -27,30 +29,57 @@ public class PersonControllerTest {
     }
 
     @Test
-    public void usernameFailedTest() {
-
+    public void createAndDeleteUser() {
+        JSONObject requestParams = new JSONObject(); 
+        try{
+            requestParams.put("username", "testRandom");
+            requestParams.put("type", "admin");
+            requestParams.put("password", "1");
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
         // Send request and receive response
         Response response = RestAssured.given().
-                header("Content-Type", "text/plain").
-                header("charset","utf-8").
-                body("").
+                header("Content-Type","application/json" ).
+                header("Accept","application/json" ).
+                body(requestParams.toString()).
                 when().
-                get("/people");
+                post("/people/post");
 
 
         // Check status code
         int statusCode = response.getStatusCode();
         assertEquals(200, statusCode);
 
-        /** 
         // Check response body for correct response
         String returnString = response.getBody().asString();
         try {
             JSONObject returnObj = new JSONObject(returnString); 
-            assertEquals("failed", returnObj.get("username"));
+            //assertEquals("failed", returnObj.get("username"));
+            assertEquals(requestParams.get("username"), returnObj.get("username"));
+            assertEquals(requestParams.get("password"), returnObj.get("password"));
+            assertEquals( 10000.0, returnObj.get("cashValue"));
+            assertEquals(10000.0, returnObj.get("totalValue"));
+            assertEquals("admin", returnObj.get("type"));
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        **/
+        Response del = RestAssured.given().
+            header("Content-Type","application/json" ).
+            header("Accept","application/json" ).
+            body("").
+            when().
+            delete("/person/delete/{username}", "testRandom");
+
+        assertEquals(200, del.getStatusCode());
+
+        String delReturn = del.getBody().asString();
+        System.out.println(delReturn);
+        try{
+            JSONObject retObject = new JSONObject(delReturn);
+            assertEquals(true, retObject.get("deleted"));
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
     } 
 }
