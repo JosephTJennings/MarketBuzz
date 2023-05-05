a7bd8477b1aefcdbc9e6dc385362a06863ab7445
Sam Husted <samuelhusted7@gmail.com>
2023-05-05 12:30:54 -0500

    Added oyur moom

--- Frontend/app/src/main/java/com/example/stockproject/Activities/OptionsActivity.java
+++ Frontend/app/src/main/java/com/example/stockproject/Activities/OptionsActivity.java
@@ -4,17 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.stockproject.Activities.model.StocksModel;
 import com.example.stockproject.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import app.server.Const;
+import app.utils.BasicUtils;
 
 /**
  * This is the Options Activity. This activity is meant for changing settings or logging out.
  */
 public class OptionsActivity extends AppCompatActivity {
-    private Button HomeButton;
+    private Button HomeButton, DeleteButton;
+    private EditText deleteUser;
     private String currentUser, currentType, currentMoney, currentValuation;
+    private RequestQueue volleyQueue;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
@@ -32,7 +53,9 @@ public class OptionsActivity extends AppCompatActivity {
         if (currentType == null) {
             currentType = "Admin";
         }
-
+        volleyQueue = Volley.newRequestQueue(OptionsActivity.this);
+        deleteUser = (EditText) findViewById(R.id.deletedUser);
+        DeleteButton = (Button) findViewById(R.id.DeleteUserButton);
         HomeButton = (Button) findViewById(R.id.home_button5);
         HomeButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -46,5 +69,43 @@ public class OptionsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        DeleteButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (String.valueOf(deleteUser.getText()).equals(currentUser)) {
+                    String successMessage = "Cannot delete current user " + currentUser + ".";
+                    Toast.makeText(OptionsActivity.this, successMessage, Toast.LENGTH_SHORT + 1).show();
+                }
+                else deleteUser();
+            }
+        });
+    }
+    private void deleteUser() {
+        String delUser = String.valueOf(deleteUser.getText());
+        String url = Const.URL + "/person/delete/" + delUser;
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.DELETE, url, null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        if (response.getBoolean("deleted") == true) {
+                            String successMessage = "Deleted " + String.valueOf(deleteUser.getText()) + ".";
+                            Toast.makeText(OptionsActivity.this, successMessage, Toast.LENGTH_SHORT + 1).show();
+                        }
+                        else {
+                            String successMessage = "Unable to delete " + String.valueOf(deleteUser.getText()) + ".";
+                            Toast.makeText(OptionsActivity.this, successMessage, Toast.LENGTH_SHORT + 1).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        volleyQueue.add(request);
     }
 }
