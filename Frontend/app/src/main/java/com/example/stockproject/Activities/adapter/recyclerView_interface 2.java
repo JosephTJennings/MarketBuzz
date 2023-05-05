44c5cfecbb41f5801830f67dd56b8d6ac9bbef70
Joseph Jennings <jtj9@iastate.edu>
2023-05-05 01:57:30 +0000

    Revert "Commiting EVERU Change"
    This reverts commit 9db923de9a1287e653fe1111b24c4281a9ab58bc

--- Frontend/app/src/main/java/com/example/stockproject/Activities/adapter/recyclerView_interface 2.java
+++
@@ -1,8 +0,0 @@
-package com.example.stockproject.Activities.adapter;
-
-/**
- * This class is a simple interface for when users Click on an element in a recyclerView.
- */
-public interface recyclerView_interface {
-    void onItemClick(int position);
-}
